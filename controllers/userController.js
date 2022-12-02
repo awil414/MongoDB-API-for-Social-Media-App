b849a997b03c1128bad9912ec0a0ac93535360bc
Amber Wilson <amberwilson@MacBook-Pro.local>
2022-12-02 13:58:41 -0600

    adding removeFriend to userController

--- controllers/userController.js
+++ controllers/userController.js
@@ -110,7 +110,24 @@ module.exports = {
                 )
                 .catch((err) => res.status(500).json(err));
     },
-
+    // Remove friend from a user
+    removeFriend(req, res) {
+        User.findOneAndUpdate(
+            { _id: req.params.userId },
+            { $pull: { friend: { friendId: req.params.friendId } } },
+            { runValidators: true, new: true }
+          )
+            .then((user) =>
+              !user
+                ? res
+                    .status(404)
+                    .json({ message: 'No user found with that ID :(' })
+                : res.json(user)
+            )
+            .catch((err) => res.status(500).json(err));
+        },
+};  
+    
 
 // // Get all users
 // const getAllUsers = async (req, res) => {
