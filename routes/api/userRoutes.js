66219d62afb2354938fdf88220db165511fe704d
Amber Wilson <amberwilson@MacBook-Pro.local>
2022-12-02 10:20:45 -0600

    changes to userRoutes

--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -7,14 +7,15 @@ const {
     getUser
 } = require('../../controllers/userController');
 
-// /api/users
-router.route('/').get(getAllUsers).post(createNewUser);
+// GET all users and POST new user /api/users
+router.route('/')
+    .get(getAllUsers)
+    .post(createNewUser);
 
-// /api/users/:userId
-router.route('/:userId').get(getUser);
-
-router.route('/:userID').get(updateUser);
-
-router.route('/:userID').get(deleteUser);
+// GET one user, PUT update a user by id, DELETE a user by id /api/users/:userId
+router.route('/:userId')
+    .get(getUser)
+    .put(updateUser)
+    .delete(deleteUser);
 
 module.exports = router;
