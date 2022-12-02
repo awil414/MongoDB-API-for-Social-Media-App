d01ab59f4e20c086bde8faba7e4ad9134b1a9409
Amber Wilson <amberwilson@MacBook-Pro.local>
2022-12-02 13:53:59 -0600

    adding addFriend to uesrController

--- controllers/userController.js
+++ controllers/userController.js
@@ -92,6 +92,25 @@ module.exports = {
         res.status(500).json(err);
       });
   },
+ // Add a friend to a user
+    addFriend(req, res) {
+        console.log('You are adding a friend.');
+        console.log(req.body);
+        User.findOneAndUpdate(
+            { _id: req.params.userId },
+            { $addToSet: { friends: req.body }},
+            { runValidators: true, new: true }
+        )
+            .then((user) =>
+                !user
+                    ? res
+                        .status(404)
+                        .json({ message: 'No user found with that ID :(' })
+                    : res.json(user)
+                )
+                .catch((err) => res.status(500).json(err));
+    },
+
 
 // // Get all users
 // const getAllUsers = async (req, res) => {
