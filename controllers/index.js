2d5541df88d4b33fe352f29ef112721a53b42ee3
Amber Wilson <amberwilson@MacBook-Pro.local>
2022-12-06 12:49:30 -0600

    deleting index.js file from controllers

--- controllers/index.js
+++
@@ -1,8 +0,0 @@
-// const router = require('express').Router();
-// const userRoutes = require('./userController');
-// const thoughtRoutes = require('./thoughtController');
-
-// router.use('/api/users', userRoutes);
-// router.use('/api/thoughts', thoughtRoutes);
-
-// module.exports = router;
